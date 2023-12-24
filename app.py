e1555ed6921aac729464d9ed3b53560d046be6b8
vasnu <154647578+vasnu@users.noreply.github.com>
2023-12-24 09:50:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'GrryMatters'
 
+def hello_world():
+   
+    return 'LazyDeveloper'
 
 if __name__ == "__main__":
+ 
     app.run()
